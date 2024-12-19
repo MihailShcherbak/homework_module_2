2ae15caa16db37be4df16120d61b58e871a0db2e
MihailShcherbak <pochta.mihaila@gmail.com>
2024-12-20 00:16:12 +0300

    19.12.2024

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,9 @@
-first=int(input('Введите первое число: '))
-second=int(input('Введите второе число: '))
-third=int(input('Введите третье число: '))
-if first==second and first==third:
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+if first == second and first == third:
     print('Одинаковых чисел: 3')
-elif first==second or first==third or second==third:
+elif first == second or first == third or second == third:
     print('Одинаковых чисел: 2')
 else:
     print('Одинаковых чисел: 0')
